e4dbe7b87d352e35ad80abbf3bab9ad896e1dcb0
N3002 User <N3002user@N3002.uc-bcf.edu.ph>
2024-02-14 14:00:55 +0800

    added buttons

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <a href="/AboutMe"><Button>About Me</Button></a>
+    <a href="/Skills"><Button>Skills</Button></a>
+    <a href="/Hobbies"><Button>Hobbies</Button></a>
+</body>
+</html>
---
+++ resources/views/Skills.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Skills</title>
+</head>
+<body>
+    <h1>Skills</h1>
+    <a href="/Home"><button>Home</button></a>
+    <br>
+    <a href="/AboutMe"><Button>About Me</Button></a>
+    <a href="/Skills"><Button>Skills</Button></a>
+    <a href="/Hobbies"><Button>Hobbies</Button></a>
+</body>
+</html>
